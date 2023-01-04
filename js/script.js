a6ef93bec698b16d05e0d2f833dad16b650709a1
SunthoDeve <sunthodeb7878@gmail.com>
2023-01-05 01:09:19 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+
+
+
+
+// counter jaquery pluging start
+
+
+
+    $('.counter').counterUp();
+
+
+
+// counter jaquery pluging end
+
+// barfiller jaquery pluging start
+
+
+    $(document).ready(function(){
+        $('#bar1').barfiller();
+    });
+
+    $(document).ready(function(){
+        $('#bar2').barfiller();
+    });
+
+    $(document).ready(function(){
+        $('#bar3').barfiller();
+    });
+
+    $(document).ready(function(){
+        $('#bar4').barfiller();
+    });
+
+    $(document).ready(function(){
+        $('#bar5').barfiller();
+    });
+
+    $(document).ready(function(){
+        $('#bar6').barfiller();
+    });
+
+
+
+// barfiller jaquery pluging start
+
+
+// slick slider jaquery pluging start
+
+
+$('.all_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow:false,
+    nextArrow:false,
+    dots:true,
+  });
+      
+
+  
+// slick slider jaquery pluging start
+
+
+$('.slider_item').slick({
+    slidesToShow:5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:false,
+    nextArrow:false,
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
